0cd84413a678dffbd47ef688468b287c654a47c8
Franck Duval <10franckpertinent@gmail.com>
2025-04-14 20:54:18 +0200

    first version with css

--- www/index.php
+++ www/index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Accueil</title>
+    <link rel="stylesheet" href="./assets/main.css">
 </head>
 <body>
     <b>Bienvenue Au debut du projet 2025</b>
