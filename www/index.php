82323bdcdcc4460f25914289a54a70461dee6953
Franck Duval <10franckpertinent@gmail.com>
2025-04-14 13:39:17 +0200

    laaste versi with all view

---
+++ www/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil</title>
+</head>
+<body>
+    <b>Bienvenue Au debut du projet 2025</b>
+    <?php 
+        #Test de la connexion à la base de données
+        new PDO("mysql:host=db;dbname=group05", "group05", "group05");
+    ?>
+</body>
+</html>
